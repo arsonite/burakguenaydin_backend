bd43e23224d461aea7851b12e9f94ccfd1526a7c
arsonite <tr_burak@hotmail.de>
2019-02-25 10:56:24 +0100

    bugfix

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const config = require('config');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+require('winston-mongodb');
+
 const app = express();
 
 /* Routes requirements */
@@ -17,6 +19,31 @@ app.use(bodyParser.json());
 /* Middlewares */
 
 /* Connecting mongoose to database */
+winston.handleExceptions(
+  new winston.transports.Console({ colorize: true, prettyPrint: true }),
+  new winston.transports.File({ filename: 'uncaughtExceptions.log' })
+);
+
+// unhandled promise rejections
+process.on('unhandledRejection', ex => {
+  throw ex;
+});
+
+winston.add(winston.transports.File, { filename: 'logfile.log' });
+winston.add(winston.transports.MongoDB, {
+  db: config.get('log'),
+  collection: 'logs',
+  level: 'info'
+});
+winston.info(`Logging to ${config.get('log')}...`);
+
+if (!config.get('jwtPrivateKey')) {
+  // momentan wird der Private Key direkt aus default/development.json gelesen (in Klartext)
+  // später soll eine Umgebungsvariable auf dem Server gesetzt werden und mit
+  // custom-environment-variables.json auf die Umgebungsvariable gemappt werden
+  throw new Error('FATAL ERROR: jwtPrivateKey is not defined.');
+}
+
 const db = config.get('data');
 mongoose
   .connect(db, {
