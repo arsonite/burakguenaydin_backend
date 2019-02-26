c281e4e652194ed47b9f426c1ca5646419aa636d
arsonite <tr_burak@hotmail.de>
2019-02-26 14:03:28 +0100

    fixed typo

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const config = require('config');
 
 const app = express();
 
